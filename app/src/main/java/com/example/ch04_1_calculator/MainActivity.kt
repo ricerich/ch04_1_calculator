e688e3abe8a805979550f687ea51a2b487bc2de2
ricerich <ricerich@gmail.com>
2021-10-21 11:46:58 +0900

    5장 버튼 계산기로 업글

--- app/src/main/java/com/example/ch04_1_calculator/MainActivity.kt
+++ app/src/main/java/com/example/ch04_1_calculator/MainActivity.kt
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_table)
 
         //1.java 객체 만들기, 2.바인딩까지
         var edt1 : EditText = findViewById<EditText>(R.id.Edit1)
@@ -36,7 +37,73 @@ class MainActivity : AppCompatActivity()
         var btn5 : Button = findViewById<Button>(R.id.BtnRem)
 
         var tv1 : TextView = findViewById<TextView>(R.id.TextResult)
-        
+
+//        var btn00 : Button = findViewById<Button>(R.id.BtnNum0)
+//        var btn01 : Button = findViewById<Button>(R.id.BtnNum1)
+//        var btn02 : Button = findViewById<Button>(R.id.BtnNum2)
+//        var btn03 : Button = findViewById<Button>(R.id.BtnNum3)
+//        var btn04 : Button = findViewById<Button>(R.id.BtnNum4)
+//        var btn05 : Button = findViewById<Button>(R.id.BtnNum5)
+//        var btn06 : Button = findViewById<Button>(R.id.BtnNum6)
+//        var btn07 : Button = findViewById<Button>(R.id.BtnNum7)
+//        var btn08 : Button = findViewById<Button>(R.id.BtnNum8)
+//        var btn09 : Button = findViewById<Button>(R.id.BtnNum9)
+
+        var btnNumArray = ArrayList<Button>()//배열 아님 -> ArrayList
+
+//        btnNumArray[0] = findViewById<Button>(R.id.BtnNum0)
+//        btnNumArray[1] = findViewById<Button>(R.id.BtnNum1)
+//        btnNumArray[2] = findViewById<Button>(R.id.BtnNum2)
+
+        var btnIdArray = arrayOf(R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3, R.id.BtnNum4,
+                                 R.id.BtnNum5, R.id.BtnNum6, R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9)
+
+        for(i in 0..btnIdArray.size-1 step 1) {
+//            btnNumArray[i] = findViewById<Button>(R.id.BtnNum0)
+//            btnNumArray[i] = findViewById<Button>(btnIdArray[i])// 일반 배열일 때 값을 담는 방법, ArrayList는 이렇게 하면 안됨
+            btnNumArray.add(findViewById<Button>(btnIdArray[i]))// 일반 배열일 때 값을 담는 방법, ArrayList는 이렇게 하면 안됨
+        }
+//        btn00.setOnClickListener {
+//            if(edt1.isFocused)
+//            {
+//                var num = edt1.text.toString() + btn00.text.toString()
+//                edt1.setText(num)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var num = edt2.text.toString() + btn00.text.toString()
+//                edt2.setText(num)
+//            }
+//        }
+//        btn01.setOnClickListener {
+//            if(edt1.isFocused)
+//            {
+//                var num = edt1.text.toString() + btn01.text.toString()
+//                edt1.setText(num)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var num = edt2.text.toString() + btn01.text.toString()
+//                edt2.setText(num)
+//            }
+//        }
+
+        for(i in 0..btnNumArray.size-1 step 1)
+        {
+            btnNumArray[i].setOnClickListener {
+                if (edt1.isFocused) {
+                    var num = edt1.text.toString() + btnNumArray[i].text.toString()
+                    edt1.setText(num)
+                } else if (edt2.isFocused) {
+                    var num = edt2.text.toString() + btnNumArray[i].text.toString()
+                    edt2.setText(num)
+                }
+                else
+                    Toast.makeText(applicationContext,"에딧을 선택해주세요!",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
         //3.이벤트 처리(버튼) -> 에딧텍스트에서 값 가져와서, -> result 텍스트뷰에 출력
         btn1.setOnTouchListener { v, motionEvent ->
 
